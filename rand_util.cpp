9c4029063cc9d3d358590254a0a38efc82dc9e71
antiquark2 <20760002+antiquark2@users.noreply.github.com>
2022-08-21 22:20:57 -0500

    Update rand_util.cpp

--- rand_util.cpp
+++ rand_util.cpp
@@ -1,6 +1,15 @@
 #include <random>
+#include <tuple>
 
 namespace rand_util 
 {
-std::default_random_engine rand_util_generator;
+std::tuple<std::mt19937       ,
+           std::minstd_rand0  ,
+           std::minstd_rand   ,
+           std::mt19937_64    ,
+           std::ranlux24_base ,
+           std::ranlux48_base ,
+           std::ranlux24      ,
+           std::ranlux48      ,
+           std::knuth_b> rand_generators; 
 } 
